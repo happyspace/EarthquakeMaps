905c384a75223abc10013b9c82d7db26352e29d2
Eddie Warner <happyspace@gmail.com>
2016-07-01 15:02:10 -0700

    create initial maven project with correct jars.

---
+++ src/main/java/com/happyspace/earthquake/App.java
@@ -0,0 +1,13 @@
+package com.happyspace.earthquake;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
